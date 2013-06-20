5cb9c930e9e4087249a8718c84eabc20bcbdf0f1
saurabh <dinsaurabh123@gmail.com>
2013-06-20 12:37:09 -0500

    Print a.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,11 +2,12 @@ package com.test;
 
 public class Test {
 	
-	private int a;
+	private static int a;
 	private int b;
 	
 	public static void main(String[] args) {
 		
+		System.out.println(a);
 	}
 
 }
