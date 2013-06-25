47a09b461f4d1cc4935872cbae921b0f001c560c
saurabh <dinsaurabh123@gmail.com>
2013-06-25 13:07:40 -0500

    Added c.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,9 @@ public class Test {
 	
 	private static int a;
 	private static int b;
+	private static int c;
 	private static int d;
+	
 	
 	public static void main(String[] args) {
 		System.out.println(b);
