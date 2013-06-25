35fdf379811c7d05b9934c7f90c9a3a4c33e8308
saurabh <dinsaurabh123@gmail.com>
2013-06-25 16:27:24 -0500

    Replaced d with e.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	private static int a;
 	private static int b;
 	private static int c;
-	private static int d;
+	private static int e;
 	
 	
 	public static void main(String[] args) {
