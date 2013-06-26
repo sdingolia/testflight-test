f85a5298c7ffa5654765950678bf83a0194a8380
saurabh <dinsaurabh123@gmail.com>
2013-06-25 19:28:50 -0500

    Removed two variables.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,9 +4,6 @@ public class Test {
 	
 	private static int e;
 	private static int b;
-	private static int q;
-	private static int w;
-	
 	
 	public static void main(String[] args) {
 		System.out.println(b);
