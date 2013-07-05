2591332e08e1e8bf3846f7eb2490f5a03986379d
saurabh <dinsaurabh123@gmail.com>
2013-07-05 17:11:40 -0500

    Commit 35.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	private static int e;
 	private static int w;
-	private static int z;
+	private static int q;
 	
 	public static void main(String[] args) {
 		System.out.println(w);
