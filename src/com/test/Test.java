8a1b424c0736ab9a1832f929bc59375daffa9df4
saurabh <dinsaurabh123@gmail.com>
2013-07-05 17:49:44 -0500

    Corrected error.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,7 +3,7 @@ package com.test;
 public class Test {
 	
 	private static int e;
-	private static int q;
+	private static int w;
 	
 	public static void main(String[] args) {
 		System.out.println(w);
