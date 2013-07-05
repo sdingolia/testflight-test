8cf28f3dd801c43fa7368bdd097192d8cdb49da1
saurabh <dinsaurabh123@gmail.com>
2013-07-05 17:05:31 -0500

    Comii 34.

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	private static int e;
 	private static int w;
-	private static int r;
+	private static int z;
 	
 	public static void main(String[] args) {
 		System.out.println(w);
